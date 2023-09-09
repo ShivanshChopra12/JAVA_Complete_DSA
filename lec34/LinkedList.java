356f4fdab52e2c2824ae3e2b1bd138909c4cacc2
Shivansh <102555294+ShivanshChopra12@users.noreply.github.com>
2023-09-10 01:56:57 +0530

    Add files via upload

--- lec34/LinkedList.java
+++ lec34/LinkedList.java
@@ -284,4 +284,19 @@ public class LinkedList {
 	        }
 	        return c1;
 	    }
+	 
+	 //leetcode 141
+	 public boolean hasCycle(Node head) {
+	        Node fast = head;
+	        Node slow = head;
+	        
+	        while(fast!=null && fast.next != null){
+	            fast =  fast.next.next;
+	            slow = slow.next;
+	            if(fast==slow){
+	                return true;
+	            }
+	        }
+	        return false;
+	    }
 }
