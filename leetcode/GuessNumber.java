98bd4707c928235915ddee7ead46765b324df480
Shivansh <102555294+ShivanshChopra12@users.noreply.github.com>
2023-09-12 02:45:36 +0530

    ques ++

---
+++ leetcode/GuessNumber.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+public class GuessNumber {
+	/** 
+	 * Forward declaration of guess API.
+	 * @param  num   your guess
+	 * @return 	     -1 if num is higher than the picked number
+	 *			      1 if num is lower than the picked number
+	 *               otherwise return 0
+	 * int guess(int num);
+	 */
+
+	public class Solution {
+	    public int guessNumber(int n) {
+	        int start = 1 , end = n;
+	      while(start <= end){
+	        int mid = start +(end - start)/2;
+	        if(guess(mid)==0) return mid;   
+	        else if(guess(mid)==1) start=mid+1;
+	        else end=mid-1;  
+	      } 
+	        return -1;
+	    }
+	}
+
+	public int guess(int mid) {
+		//given in leetcode
+		return 0;
+	}
+}
